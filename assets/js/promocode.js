9e94c0a7821ee326785db3a83260775ef680d149
SWKG <140810203+SWKGSTUDIO@users.noreply.github.com>
2023-08-25 10:46:58 +0300

    promocode generation

--- assets/js/promocode.js
+++ assets/js/promocode.js
@@ -1,40 +1,89 @@
-// Функция для генерации случайной латинской буквы
+// Function to generate a random letter
 function getRandomLetter() {
-    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    return letters[Math.floor(Math.random() * letters.length)];
+  const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  return letters[Math.floor(Math.random() * letters.length)];
 }
 
-// Функция для генерации случайного числа или нуля
+// Function to generate a random number or zero
 function getRandomNumberOrZero() {
-    return Math.random() < 0.5 ? '0' : Math.floor(Math.random() * 10).toString();
+  return Math.random() < 0.5 ? '0' : Math.floor(Math.random() * 10).toString();
 }
 
-// Функция для генерации промокода
+// Function to generate a promo code
 function generatePromoCode() {
-    const promoCode = `SWKG-${getRandomLetter()}${getRandomNumberOrZero()}${getRandomNumberOrZero()}${getRandomLetter()}-${getRandomNumberOrZero()}${getRandomLetter()}${getRandomNumberOrZero()}${getRandomNumberOrZero()}`;
-    return promoCode;
+  const promoCode = `SWKG-${getRandomLetter()}${getRandomNumberOrZero()}${getRandomNumberOrZero()}${getRandomLetter()}-${getRandomNumberOrZero()}${getRandomLetter()}${getRandomNumberOrZero()}${getRandomNumberOrZero()}`;
+  return promoCode;
 }
 
-// Обработчик нажатия на кнопку
-document.getElementById('generateButton').addEventListener('click', function() {
-    const promoCode = generatePromoCode();
-    // alert(`Generated Promo Code: ${promoCode}`);
-    this.innerText = promoCode;
-    // Здесь можно отправить promoCode на сервер для сохранения, например, через AJAX запрос
-    // Пример:
-    /*
-    fetch('/save-promo-code', {
-        method: 'POST',
-        body: JSON.stringify({ promoCode }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then(response => {
-        if (response.ok) {
-            console.log('Promo Code saved successfully.');
-        } else {
-            console.error('Failed to save Promo Code.');
-        }
+// Определение функции для получения и отображения промокода
+function fetchAndDisplayPromoCode() {
+  const promoCodeDisplay = document.getElementById('promoCodeDisplay');
+
+  fetch('https://intermediate-easy-ship.glitch.me/get-promo-code')
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        console.error('Failed to fetch Promo Code.');
+      }
+    }).then(data => {
+      if (data && data.promoCode) {
+        promoCodeDisplay.innerText = `Generated Promo Code: ${data.promoCode}`;
+      } else {
+        console.error('No promo code data received from server.');
+      }
+    }).catch(error => {
+      console.error('Error fetching promo code:', error);
     });
-    */
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const generateButton = document.getElementById('generateButton');
+  const promoCodeDisplay = document.getElementById('promoCodeDisplay');
+    
+  fetchAndDisplayPromoCode();
+
+
+  // Event listener for the button click
+  generateButton.addEventListener('click', function() {
+    const promoCode = generatePromoCode();
+
+    // Send promoCode to the server
+    fetch('https://intermediate-easy-ship.glitch.me/save-promo-code', {
+      method: 'POST',
+      body: JSON.stringify({ promoCode }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        console.error('Failed to save Promo Code.');
+      }
+    }).then(data => {
+      if (data && data.message === 'Promo code saved successfully') {
+        // Fetch the promo code from the server
+        fetch('https://intermediate-easy-ship.glitch.me/get-promo-code')
+          .then(response => {
+            if (response.ok) {
+              return response.json();
+            } else {
+              console.error('Failed to fetch Promo Code.');
+            }
+          }).then(data => {
+            if (data && data.promoCode) {
+              promoCodeDisplay.innerText = `Generated Promo Code: ${data.promoCode}`;
+            } else {
+              promoCodeDisplay.innerText = 'No promo code available.';
+            }
+          }).catch(error => {
+            console.error('Error fetching promo code:', error);
+          });
+      }
+    }).catch(error => {
+      console.error('Error fetching promo code:', error);
+    });
+  });
 });

578190ea4d513e6806e70a7098a0d3dc537749ce
SWKG <140810203+SWKGSTUDIO@users.noreply.github.com>
2023-09-29 09:10:39 +0300

    updAdminka

--- script.js
+++ script.js
@@ -143,41 +143,45 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function loadTabContent(game) {
 
-    
 
         tabContent.innerHTML = '';
     
         const tabPanel = document.createElement('div');
         tabPanel.classList.add('tab-panel');
     
+
         const saveButton = document.createElement('button');
         saveButton.textContent = 'Сохранить';
         saveButton.id = 'save-button';
-    
+        tabPanel.appendChild(saveButton);
         const saveIcon = document.createElement('i');
         saveIcon.classList.add('fas', 'fa-save');
-
+        saveButton.appendChild(saveIcon);
         // Создаем элемент прогресс-бара
         const progressBar = document.createElement('div');
         progressBar.classList.add('progress-bar');
         progressBar.style.width = '0%';
+        saveButton.appendChild(progressBar);
+
     
         const gameNameLabel = document.createElement('label');
         gameNameLabel.textContent = 'Название:';
-    
         const gameNameInput = document.createElement('input');
         gameNameInput.type = 'text';
         gameNameInput.value = game.name;
         gameNameInput.id = 'game-name-input';
+        tabPanel.appendChild(gameNameLabel);
+        tabPanel.appendChild(gameNameInput);
 
 
-        const gameAppNameLabel = document.createElement('label');
-        gameAppNameLabel.textContent = 'App Название:';
-    
-        const gameAppNameInput = document.createElement('input');
-        gameAppNameInput.type = 'text';
-        gameAppNameInput.value = game.appname;
-        gameAppNameInput.id = 'gameApp-name-input';
+        const GooglePlayLabel = document.createElement('label');
+        GooglePlayLabel.textContent = 'GOOGLE PLAY:';
+        const GooglePlayInput = document.createElement('input');
+        GooglePlayInput.type = 'text';
+        GooglePlayInput.value = game.googleplay;
+        GooglePlayInput.id = 'googlePlay-name-input';
+        tabPanel.appendChild(GooglePlayLabel);
+        tabPanel.appendChild(GooglePlayInput);
 
 
 
@@ -187,118 +191,149 @@ document.addEventListener('DOMContentLoaded', () => {
         ComAppInput.type = 'text';
         ComAppInput.value = game.comapp;
         ComAppInput.id = 'comApp-name-input';
+        tabPanel.appendChild(ComAppLabel);
+        tabPanel.appendChild(ComAppInput);
 
 
-        const GooglePlayLabel = document.createElement('label');
-        GooglePlayLabel.textContent = 'GOOGLE PLAY:';
-        const GooglePlayInput = document.createElement('input');
-        GooglePlayInput.type = 'text';
-        GooglePlayInput.value = game.googleplay;
-        GooglePlayInput.id = 'googlePlay-name-input';
+        const linkLiteLabel = document.createElement('label');
+        linkLiteLabel.textContent = 'Ссылка Lite:';
+        const linkLiteInput = document.createElement('input');
+        linkLiteInput.type = 'text';
+        linkLiteInput.value = game.link_lite || '';
+        linkLiteInput.id = 'link-lite-input';
+        tabPanel.appendChild(linkLiteLabel);
+        tabPanel.appendChild(linkLiteInput);
+        
 
+        const linkPremiumLabel = document.createElement('label');
+        linkPremiumLabel.textContent = 'Ссылка Premium:';
+        const linkPremiumInput = document.createElement('input');
+        linkPremiumInput.type = 'text';
+        linkPremiumInput.value = game.link_premium || '';
+        linkPremiumInput.id = 'link-premium-input';
+        tabPanel.appendChild(linkPremiumLabel);
+        tabPanel.appendChild(linkPremiumInput);
+
+       
+
+        const gameAppNameLabel = document.createElement('label');
+        gameAppNameLabel.textContent = 'App Название:';
+        const gameAppNameInput = document.createElement('input');
+        gameAppNameInput.type = 'text';
+        gameAppNameInput.value = game.appname;
+        gameAppNameInput.id = 'gameApp-name-input';
+        tabPanel.appendChild(gameAppNameLabel);
+        tabPanel.appendChild(gameAppNameInput);
 
 
     
         const googleAdMobLabel = document.createElement('label');
         googleAdMobLabel.textContent = 'AdMob AppID:';
-    
         const googleAdMobInput = document.createElement('input');
         googleAdMobInput.type = 'text';
         googleAdMobInput.value = game.google_admob_id || '';
         googleAdMobInput.id = 'google-admob-input';
+        tabPanel.appendChild(googleAdMobLabel);
+        tabPanel.appendChild(googleAdMobInput);
+
+
     
         const bannerIdLabel = document.createElement('label');
         bannerIdLabel.textContent = 'Баннер ИД:';
-    
         const bannerIdInput = document.createElement('input');
         bannerIdInput.type = 'text';
         bannerIdInput.value = game.banner_id || '';
         bannerIdInput.id = 'banner-id-input';
-    
+        tabPanel.appendChild(bannerIdLabel);
+        tabPanel.appendChild(bannerIdInput);
+
+
+
         const interstitialIdLabel = document.createElement('label');
         interstitialIdLabel.textContent = 'Межстраничка ИД:';
-    
         const interstitialIdInput = document.createElement('input');
         interstitialIdInput.type = 'text';
         interstitialIdInput.value = game.interstitial_id || '';
         interstitialIdInput.id = 'interstitial-id-input';
-    
+        tabPanel.appendChild(interstitialIdLabel);
+        tabPanel.appendChild(interstitialIdInput);
+
+
         const rewardIdLabel = document.createElement('label');
         rewardIdLabel.textContent = 'Ревард ИД:';
-    
         const rewardIdInput = document.createElement('input');
         rewardIdInput.type = 'text';
         rewardIdInput.value = game.reward_id || '';
         rewardIdInput.id = 'reward-id-input';
+        tabPanel.appendChild(rewardIdLabel);
+        tabPanel.appendChild(rewardIdInput);
+
 
 
         const promoLiteLabel = document.createElement('label');
         promoLiteLabel.textContent = 'Промо-код Lite:';
-
         const promoLiteInput = document.createElement('input');
         promoLiteInput.type = 'text';
-        promoLiteInput.value = game.promo_lite || ''; // Предположим, что поле называется promo_lite
+        promoLiteInput.value = game.promo_lite || '';
         promoLiteInput.id = 'promo-lite-input';
+        tabPanel.appendChild(promoLiteLabel);
+        tabPanel.appendChild(promoLiteInput);
+
+
 
         const promoPremiumLabel = document.createElement('label');
         promoPremiumLabel.textContent = 'Промо-код Premium:';
-
         const promoPremiumInput = document.createElement('input');
         promoPremiumInput.type = 'text';
-        promoPremiumInput.value = game.promo_premium || ''; // Предположим, что поле называется promo_premium
+        promoPremiumInput.value = game.promo_premium || '';
         promoPremiumInput.id = 'promo-premium-input';
+        tabPanel.appendChild(promoPremiumLabel);
+        tabPanel.appendChild(promoPremiumInput);
+        
 
 
         const installZipLabel = document.createElement('label');
         installZipLabel.textContent = 'Install Zip:';
-
         const installZipInput = document.createElement('input');
         installZipInput.type = 'text';
         installZipInput.value = game.install_zip || '';
         installZipInput.id = 'install-zip-input';
+        tabPanel.appendChild(installZipLabel);
+        tabPanel.appendChild(installZipInput);
+
 
         const installZipFreeLabel = document.createElement('label');
         installZipFreeLabel.textContent = 'Install Zip Free:';
-
         const installZipFreeInput = document.createElement('input');
         installZipFreeInput.type = 'text';
         installZipFreeInput.value = game.install_zip_free || '';
         installZipFreeInput.id = 'install-zip-free-input';
+        tabPanel.appendChild(installZipFreeLabel);
+        tabPanel.appendChild(installZipFreeInput);
 
 
-        const linkLiteLabel = document.createElement('label');
-        linkLiteLabel.textContent = 'Ссылка Lite:';
-
-        const linkLiteInput = document.createElement('input');
-        linkLiteInput.type = 'text';
-        linkLiteInput.value = game.link_lite || '';
-        linkLiteInput.id = 'link-lite-input';
-
-        const linkPremiumLabel = document.createElement('label');
-        linkPremiumLabel.textContent = 'Ссылка Premium:';
-
-        const linkPremiumInput = document.createElement('input');
-        linkPremiumInput.type = 'text';
-        linkPremiumInput.value = game.link_premium || '';
-        linkPremiumInput.id = 'link-premium-input';
 
         const intentLabel = document.createElement('label');
         intentLabel.textContent = 'Intent:';
-
         const intentInput = document.createElement('input');
         intentInput.type = 'text';
         intentInput.value = game.intent || '';
         intentInput.id = 'intent-input';
+        tabPanel.appendChild(intentLabel);
+        tabPanel.appendChild(intentInput);
 
 
 
         // Создайте элементы для ввода строкового кода
         const stringCodeLabel = document.createElement('label');
         stringCodeLabel.textContent = 'Код (строка):';
-
         const stringCodeTextarea = document.createElement('textarea');
         stringCodeTextarea.value = game.string_code || '';
-    
+        tabPanel.appendChild(stringCodeLabel);
+        tabPanel.appendChild(stringCodeTextarea);
+
+
+
         stringCodeTextarea.addEventListener('input', () => {
             const newStringCode = stringCodeTextarea.value;
             game.string_code = newStringCode;
@@ -307,8 +342,9 @@ document.addEventListener('DOMContentLoaded', () => {
         const generateCodeButton = document.createElement('button');
         generateCodeButton.textContent = 'Сгенерировать код';
         generateCodeButton.classList.add('generate-code-button');
+        tabPanel.appendChild(generateCodeButton);
 
-    
+
         generateCodeButton.addEventListener('click', () => {
             const app_name = gameAppNameInput.value;
             const admob_app_id = googleAdMobInput.value;
@@ -339,108 +375,74 @@ document.addEventListener('DOMContentLoaded', () => {
             stringCodeTextarea.value = generatedCode;
         });
         
+    
+        // tabPanel.appendChild(promoLiteLabel);
+        // tabPanel.appendChild(promoLiteInput);
 
-        tabPanel.appendChild(saveButton);
-        saveButton.appendChild(progressBar);
-        saveButton.appendChild(saveIcon);
+        // tabPanel.appendChild(promoPremiumLabel);
+        // tabPanel.appendChild(promoPremiumInput);
 
-        tabPanel.appendChild(gameNameLabel);
-        tabPanel.appendChild(gameNameInput);
-
-        tabPanel.appendChild(GooglePlayLabel);
-        tabPanel.appendChild(GooglePlayInput);
-
-        tabPanel.appendChild(ComAppLabel);
-        tabPanel.appendChild(ComAppInput);
-
-
-        tabPanel.appendChild(linkLiteLabel);
-        tabPanel.appendChild(linkLiteInput);
-        tabPanel.appendChild(linkPremiumLabel);
-        tabPanel.appendChild(linkPremiumInput);
-
-       
-        tabPanel.appendChild(gameAppNameLabel);
-        tabPanel.appendChild(gameAppNameInput);
-
-        tabPanel.appendChild(googleAdMobLabel);
-        tabPanel.appendChild(googleAdMobInput);
-        tabPanel.appendChild(bannerIdLabel);
-        tabPanel.appendChild(bannerIdInput);
-        tabPanel.appendChild(interstitialIdLabel);
-        tabPanel.appendChild(interstitialIdInput);
-        tabPanel.appendChild(rewardIdLabel);
-        tabPanel.appendChild(rewardIdInput);
-        tabPanel.appendChild(promoLiteLabel);
-        tabPanel.appendChild(promoLiteInput);
-        tabPanel.appendChild(promoPremiumLabel);
-        tabPanel.appendChild(promoPremiumInput);
-        tabPanel.appendChild(promoLiteLabel);
-        tabPanel.appendChild(promoLiteInput);
-        tabPanel.appendChild(promoPremiumLabel);
-        tabPanel.appendChild(promoPremiumInput);
-        tabPanel.appendChild(installZipLabel);
-        tabPanel.appendChild(installZipInput);
-        tabPanel.appendChild(installZipFreeLabel);
-        tabPanel.appendChild(installZipFreeInput);
-       
-        tabPanel.appendChild(intentLabel);
-        tabPanel.appendChild(intentInput);
-
-        tabPanel.appendChild(stringCodeLabel);
-        tabPanel.appendChild(stringCodeTextarea);
-        tabPanel.appendChild(generateCodeButton);
 
         tabContent.appendChild(tabPanel);
         
-
+        
         saveButton.addEventListener('click', async () => {
 
             progressBar.style.width = '0%';
             saveButton.textContent = 'Сохранение...';
         
             const newGameName = gameNameInput.value;
-            const newGameAppName = gameAppNameInput.value;
-
-            const newGooglePlay = GooglePlayInput.value;
-            const newComApp = ComAppInput.value;
-
-            const newGoogleAdMobId = googleAdMobInput.value;
-            const newBannerId = bannerIdInput.value;
-            const newInterstitialId = interstitialIdInput.value;
-            const newRewardId = rewardIdInput.value;
-            const newInstallZip = installZipInput.value;
-            const newInstallZipFree = installZipFreeInput.value;
-            const newPromoLite = promoLiteInput.value;
-            const newPromoPremium = promoPremiumInput.value;
-            const newLinkLite = linkLiteInput.value;
-            const newLinkPremium = linkPremiumInput.value;
-            const newIntent = intentInput.value;
-            const newStringCode = stringCodeTextarea.value;
-        
             game.name = newGameName;
+
+            const newGameAppName = gameAppNameInput.value;
             game.appname = newGameAppName;
 
-            game.comapp = newComApp;
+            const newGooglePlay = GooglePlayInput.value;
             game.googleplay = newGooglePlay;
 
+            const newComApp = ComAppInput.value;
+            game.comapp = newComApp;
 
+            const newGoogleAdMobId = googleAdMobInput.value;
             game.google_admob_id = newGoogleAdMobId;
+
+            const newBannerId = bannerIdInput.value;
             game.banner_id = newBannerId;
+
+            const newInterstitialId = interstitialIdInput.value;
             game.interstitial_id = newInterstitialId;
+
+            const newRewardId = rewardIdInput.value;
             game.reward_id = newRewardId;
+
+            const newInstallZip = installZipInput.value;
             game.install_zip = newInstallZip;
+
+            const newInstallZipFree = installZipFreeInput.value;
             game.install_zip_free = newInstallZipFree;
+
+            const newPromoLite = promoLiteInput.value;
             game.promo_lite = newPromoLite;
+
+            const newPromoPremium = promoPremiumInput.value;
             game.promo_premium = newPromoPremium;
+
+            const newLinkLite = linkLiteInput.value;
             game.link_lite = newLinkLite;
+
+            const newLinkPremium = linkPremiumInput.value;
             game.link_premium = newLinkPremium;
+
+            const newIntent = intentInput.value;
             game.intent = newIntent;
+
+            const newStringCode = stringCodeTextarea.value;
             game.string_code = newStringCode;
         
             try {
 
                  // Анимация прогресса
+                await new Promise(resolve => setTimeout(resolve, 500));
                 progressBar.style.width = '100%';
 
                 const updateResponse = await fetch(`https://intermediate-easy-ship.glitch.me/adminpaneldata/${game.id}`, {
